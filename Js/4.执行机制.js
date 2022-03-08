f599780af7a919771a4b84df4159e3bcb39721e1
ali <ali_ovo@qq.com>
2022-03-08 10:53:06 +0800

    init

--- Js/4.执行机制.js
+++ Js/4.执行机制.js
@@ -1,73 +1,77 @@
-  //  script start
-  //  a1 start
-  //  a2
-  //  promise2
-  //  script end
-  //  promise1
-  //  a1 end
-  //  promise2.then
-  //  promise3
-  // setTimeout
+//  script start
+//  a1 start
+//  a2
+//  promise2
+//  script end
+//  promise1
+//  a1 end
+//  promise2.then
+//  promise3
+// setTimeout
 
-  // async function a1() {
-  //   console.log("a1 start");
-  //   await a2();
-  //   console.log("a1 end");
-  // }
+// async function a1() {
+//   console.log("a1 start");
+//   await a2();
+//   console.log("a1 end");
+// }
 
-  // async function a2() {
-  //   console.log("a2");
-  // }
+// async function a2() {
+//   console.log("a2");
+// }
 
-  // console.log("script start");
+// console.log("script start");
 
-  // setTimeout(() => {
-  //   console.log("setTimeout");
-  // }, 0);
+// setTimeout(() => {
+//   console.log("setTimeout");
+// }, 0);
 
-  // Promise.resolve().then(() => {
-  //   console.log("promise1");
-  // });
+// Promise.resolve().then(() => {
+//   console.log("promise1");
+// });
 
-  // a1();
+// a1();
 
-  // let promise2 = new Promise((resolve) => {
-  //   resolve("promise2.then");
-  //   console.log("promise2");
-  // });
+// let promise2 = new Promise((resolve) => {
+//   resolve("promise2.then");
+//   console.log("promise2");
+// });
 
-  // promise2.then((res) => {
-  //   console.log(res);
-  //   Promise.resolve().then(() => {
-  //     console.log("promise3");
-  //   });
-  // });
+// promise2.then((res) => {
+//   console.log(res);
+//   Promise.resolve().then(() => {
+//     console.log("promise3");
+//   });
+// });
 
-  // console.log("script end");
+// console.log("script end");
 
+console.log("start");
 
+setTimeout(() => {
+  console.log("setTimeout1");
+}, 0);
 
-  console.log('begin');
+(async function foo() {
+  console.log("async 1");
+
+  await asyncFunction();
+
+  console.log("async2");
+
+})().then(console.log("foo.then"));
+
+async function asyncFunction() {
+  console.log("asyncFunction");
 
   setTimeout(() => {
-    console.log('setTimeout');
+    console.log("setTimeout2");
   }, 0);
 
-  new Promise(res => {
-    console.log('promise1');
-    res()
-  }).then(() => {
-    console.log('promise2');
-  });
+  new Promise((res) => {
+    console.log("promise1");
 
-  (async function foo() {
-    console.log('async 1');
-    await asyncFunction()
-    console.log('async2');
-  })()
+    res("promise2");
+  }).then(console.log);
+}
 
-  async function asyncFunction() {
-    console.log('asyncFunction');
-  }
-
-  console.log('end');
+console.log("end");
